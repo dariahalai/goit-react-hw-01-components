1333d445e0eb9bfcf3a544b352b2130b43019695
Daria Halai <106152100+dariahalai@users.noreply.github.com>
2022-12-18 01:00:10 +0300

    add styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css'
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).toUpperCase()}`;
+  }
+
 export const Statistics = ({title,stats}) =>{
     return (
     <section className={css.statistics}>
         <h2 className={css.title}>{title}</h2> 
         <ul className={css.statlist}>
         {stats.map(({id,label,percentage}) =>(
-                <li key={id} className={css.item}>
+                <li key={id} className={css.item} style={{backgroundColor:getRandomHexColor()}} >
                 <span className={css.label}>{label}</span>
                 <span className={css.percentage}>{percentage}%</span>
                 </li>
@@ -25,3 +29,4 @@ Statistics.propTypes ={
         percentage:PropTypes.number.isRequired
     })
 }
+
